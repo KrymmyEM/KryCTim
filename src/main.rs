887ec6fe3dbd046ffddf1c94996dda5d0ce5d2a8
Evgeny (Krymmy) Momotov <evgeny.momotov@gmail.com>
2024-03-18 17:08:47 +0300

    [UP] Timer types add

--- src/main.rs
+++ src/main.rs
@@ -6,30 +6,50 @@ fn input(var: &mut String) -> &str{
     return buf.trim();
 }
 
-fn get_time() -> (u32, u32, u32){
+fn get_time() -> (u64, u64, u64){
     let mut hour: String = String::new();
     let mut mins: String = String::new();
     let mut seconds: String = String::new();
 
-    let hour: u32 = input(&mut hour).parse().expect("Error parse number");
-    let mins: u32 = input(&mut mins).parse().expect("Error parse number");
-    let seconds: u32 = input(&mut seconds).parse().expect("Error parse number");
+    let hour: u64 = input(&mut hour).parse().expect("Error parse number");
+    let mins: u64 = input(&mut mins).parse().expect("Error parse number");
+    let seconds: u64 = input(&mut seconds).parse().expect("Error parse number");
 
     return (hour, mins, seconds);
 }
 
-fn timer(hour: u32, mins: u32, seconds: u32){
+fn timer(hour: u64, mins: u64, seconds: u64, type_timer: u8) -> bool{
+    let hour_on_seconds: u64 = hour * 3_600;
+    let mins_on_seconds: u64 = mins * 60;
+    let full_seconds: u64 = hour_on_seconds + mins_on_seconds + seconds;
+    let sleep_seconds = time::Duration::new(full_seconds, 0);
+    let second = time::Duration::new(1, 0);
 
-    loop{
-
+    match type_timer {
+        1 => {
+            return true;
+        },
+        2 => {
+            let mut counter: u64 = 1;
+            
+            while counter < full_seconds {
+                thread::sleep(second);
+                counter+=1;
+            }
+            return true;
+        },
+        _ => {
+            println!("Error type timer");
+            return false;
+        }
     }
-
 }
 
 fn main() {
+    let type_timer: u8 = input(&mut String::new()).parse().expect("Error parse number");
     loop {
         let (hour, mins, seconds) = get_time();
-        timer(hour, mins, seconds)
+        timer(hour, mins, seconds, type_timer);
 
     }
 }
